73af55511da33ed136c3f4b8c7fd65ad278bc4a3
flamingfox <julien.brillet@yahoo.fr>
2016-01-07 00:21:14 +0100

    save

--- Assets/cutscene/Ship.cs
+++ Assets/cutscene/Ship.cs
@@ -6,16 +6,23 @@ public class Ship : MonoBehaviour
 	public GameObject mesh;
 	public Animator haloAnimator;
 	public ParticleSystem effect;
+	public Animator shipAnimation;
 
 	public void spawn ()
 	{
 		effect.Play ();
+		shipAnimation.enabled = true;
 		haloAnimator.SetTrigger("active");
 		Invoke ("shipAppared", 1.0f);
 	}
 
 	private void shipAppared(){
 		mesh.SetActive (true);
+	}
+
+	public void takeOff ()
+	{
+		shipAnimation.SetTrigger ("takeOff");
 	}
 }
 
---
+++ Assets/cutscene/ShipInterface.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipInterface : MonoBehaviour {
+
+	public Ship ship;
+
+	public void SpawnShip(){
+		ship.spawn ();
+	}
+
+	public void TakeOffShip(){
+		ship.takeOff ();
+	}
+}
