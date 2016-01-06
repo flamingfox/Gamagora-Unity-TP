1b760df6ca2ab613271de84957861f74c126f23c
flamingfox <julien.brillet@yahoo.fr>
2016-01-06 07:49:51 +0100

    save tmp

--- Assets/script/Building/Buildable.cs
+++ Assets/script/Building/Buildable.cs
@@ -3,20 +3,28 @@ using System.Collections;
 
 public class Buildable : MonoBehaviour {
 
+	public bool spaceAvailable = true;
+
 	public GameObject turret_gatling;
 	public GameObject turret_missile;
 	public GameObject turret_grenade;
 
 	void OnMouseDown(){
 		//Debug.Log ("life : " + PV);
-		GameObject instance = Instantiate (turret_gatling, transform.position, transform.rotation) as GameObject;
-		instance.transform.position = transform.position + new Vector3(0f,0.5f,0f);
 
-		instance.GetComponent<Turret> ().poolingEnemy = GameManager.Instance.enemyPooling;
-		instance.GetComponent<Turret> ().buildFinish ();
+		if (spaceAvailable) {
+
+			GameObject instance = Instantiate (turret_gatling, transform.position, transform.rotation) as GameObject;
+			instance.transform.position = transform.position + new Vector3 (0f, 0.5f, 0f);
+
+			this.spaceAvailable = false;
+
+			instance.GetComponent<Turret> ().poolingEnemy = GameManager.Instance.enemyPooling;
+			instance.GetComponent<Turret> ().buildFinish ();
 
 
-		Destroy(gameObject.GetComponent<Buildable>());
+			//Destroy(gameObject.GetComponent<Buildable>());
 
+		}
 	}
 }
